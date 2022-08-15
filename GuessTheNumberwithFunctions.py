9906389c817d8d7c03410e1a6a2502101303886b
ZeckeyBay <zfatihturan@gmail.com>
2022-08-15 12:27:53 +0300

    Add files via upload

---
+++ GuessTheNumberwithFunctions.py
@@ -0,0 +1,30 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"1 ile {x} arasında bir sayı tahmin et:"))
+        if guess < random_number:
+            print("Tahminini arttır")
+        elif guess > random_number:
+            print("Tahminini azalt")
+    print(f" Tebrikler, sayıyı buldun. Sayımız: {random_number}")
+guess(100)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f"Tahmininim {guess} çok mu yüksek (H) yoksa çok mu az (L), yoksa doğrumu(C)?").lower()
+        if feedback == "h":
+             high = guess -1
+        elif feedback == "l":
+            low = guess +1
+    print(f"Tahmin ettiğin sayıyı,{guess} buldun!")
+computer_guess(100)
